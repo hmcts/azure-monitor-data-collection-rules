cc3a3e055e6db18f01e093957409ac3c584e6900
Ieuan Byers <ieuan.byers@justice.gov.uk>
2024-09-04 15:28:56 +0100

    Add provider alias

--- components/dcr/data.tf
+++ components/dcr/data.tf
@@ -1,8 +1,10 @@
 data "azurerm_resource_group" "log_analytics_rg" {
-  name = "oms-automation"
+  provider = azurerm.log_analytics
+  name     = "oms-automation"
 }
 
 data "azurerm_log_analytics_workspace" "workspace" {
+  provider            = azurerm.log_analytics
   name                = local.log_analytics_workspace
   resource_group_name = data.azurerm_resource_group.log_analytics_rg.name
 }
--- components/dcr/providers.tf
+++ components/dcr/providers.tf
@@ -10,6 +10,7 @@ terraform {
 }
 
 provider "azurerm" {
+  alias = "log_analytics"
   features {}
   subscription_id = var.log_analytics_sub_id
 }
